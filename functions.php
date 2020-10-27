dbf60ddaea20569e5aa9d02cb035961d67137b73
MeesPos <meespostma_@hotmail.com>
2020-10-27 21:40:26 +0100

    Starterfiles ingevoegd

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+function registerMenus() {
+    register_nav_menu('primary', 'Header Nav');
+}
+
+add_action('after_setup_theme', 'registerMenus');
+
+function loadStyleScript() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_style('themeStyle', get_template_directory_uri() . '/css/themeStyle.css');
+
+    wp_enqueue_script('jqueryNew', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js', array('jquery'), 3.5, true);
+    wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
+    wp_enqueue_script('bootstrapJS', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'), 4.5, true);
+    wp_enqueue_script('PopperJS', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', array('jquery'), 1.16, true);
+}
+
+add_action('init', 'loadStyleScript');
+
+function header_alt($default_alt = 'Image') {
+    $header = get_custom_header();
+    if ( ! $header ) {
+       return $default_alt;
+    }
+    return get_post_meta($header->attachment_id, '_wp_attachment_image_alt', true);
+}
+
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+
+add_action( 'init', 'register_navwalker' );
---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php echo language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name') ?></title>
+    <?php wp_head() ?>
+    <script src="https://kit.fontawesome.com/1eb7c10cba.js" crossorigin="anonymous"></script>
+</head>
+
+<body <?php body_class() ?>>
