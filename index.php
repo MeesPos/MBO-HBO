e521958b546a4312aa7ff7c54f01139e3eb6a91a
MeesPos <meespostma_@hotmail.com>
2020-10-29 17:51:07 +0100

    Navbar af en responsive

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
     <header>
-        <img src="<?php echo get_template_directory_uri() . '/img/homeHeader.jpg' ?>" alt="Leerlingen van een school - MBO HBO NHF">
+        <img class="homeHeader" src="<?php echo get_template_directory_uri() . '/img/homeHeader.jpg' ?>" alt="Leerlingen van een school - MBO HBO NHF">
         <div class="blueBorder"></div>
     </header>
 
