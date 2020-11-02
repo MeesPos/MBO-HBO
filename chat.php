e29b10651739a5a06cc349464af3ddc3427f43c6
Cornell van der Straaten <30183@ma-web.nl>
2020-11-03 00:05:03 +0100

    Desktop documenten versie done, ajax to chat page done

---
+++ chat.php
@@ -0,0 +1,3 @@
+<div class="page-part__chat-module">
+    <p>Chat page</p>
+</div>
---
+++ documenten.php
@@ -0,0 +1,29 @@
+<div class="page-part__documenten-module">
+    <div class="documenten-module__title-div">
+        <p class="documenten-module__title">ALLE DOCUMENTEN</p>
+    </div>
+
+    <div class="documenten-module__documenten-header">
+          <input type="checkbox" class="documenten-checkbox" name="documenten_checkbox" id="checkbox">
+        <p class="documenten-header-text">NAAM DOCUMENT</p>
+    </div>
+
+    <div class="documenten-module__documenten-map">
+        <input type="checkbox" name="documenten_checkbox" id="checkbox">
+        <img class="folder-icon" src="#" alt="Folder icoon" >
+        <p class="documenten-map-text">Documenten</p>
+    </div>
+    
+    <div class="documenten-module__documenten-map">
+        <input type="checkbox" name="documenten_checkbox" id="checkbox">
+        <img class="folder-icon" src="#" alt="Folder icoon">
+        <p class="documenten-map-text">Video's</p>
+    </div>
+
+    <div class="documenten-module__documenten-map">
+        <input type="checkbox" name="documenten_checkbox" id="checkbox">
+        <img class="folder-icon" src="#" alt="Folder icoon">
+        <p class="documenten-map-text">Planningen</p>
+    </div>
+
+</div>
