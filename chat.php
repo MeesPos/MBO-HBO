3119d449cd83b4ec573ef2a7c5b399c0829893df
MeesPos <meespostma_@hotmail.com>
2020-11-03 13:03:14 +0100

    Chat bug

--- chat.php
+++ chat.php
@@ -1,3 +1,13 @@
 <div class="page-part__chat-module">
-    <p>Chat page</p>
+    <div class="chatprofiles">
+        <div class="searchblock">
+            <div class="user_logo"></div>
+
+            <div class="input-group-append">
+                <button class="btn nav__search-icon" type="button">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+    </div>
 </div>
